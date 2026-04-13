b196c3750b5343f056e69f110008e059a7b9daa6
hajaww <gambasvb.bian@gmail.com>
2026-04-13 22:23:12 +0700

    Hook up State Machine enemy

---
+++ Enemies/Scripts/states/enemy_state.gd
@@ -0,0 +1,27 @@
+class_name EnemyState extends Node
+
+var enemy : Enemy
+var state_machine : EnemyStateMachine
+
+
+## What happens when we initialize this state?
+func init() -> void:
+	pass
+
+
+## What happens when the enemy enters this state?
+func enter() -> void:
+	pass
+
+
+## What happens when the enemy exits this State?
+func exit() -> void:
+	pass
+
+
+## What happens druing the _process update in this State?
+func process( _delta : float ) -> EnemyState:
+	return null
+
+func pyhsics( _delta : float ) -> EnemyState:
+	return null
