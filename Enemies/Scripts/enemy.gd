c1e3ca3c440c93aa6c058e053e6b58600b62e76e
bianbelajar <bianbelajar02@gmail.com>
2026-04-13 14:59:32 +0700

    small upd slime script

--- Enemies/Scripts/enemy.gd
+++ Enemies/Scripts/enemy.gd
@@ -1,5 +1,13 @@
-extends Node
+class_name Enemy extends CharacterBody2D
 
+signal direction_changed( new_direction : Vector2 )
+signal enemy_damaged()
+
+const DIR_4 = [ Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
+
+@export var hp : int = 3
+
+var
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
