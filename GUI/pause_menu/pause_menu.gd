90a1e40b549185a28c8ecc7954b5b50895f0d4ce
Achmad Nur Pratomo <bianbelajar@gmail.com>
2026-04-26 02:12:53 +0700

    Inventory Manage Unfinished

--- GUI/pause_menu/pause_menu.gd
+++ GUI/pause_menu/pause_menu.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
-@onready var button_save: Button = $VBoxContainer/Button_Save
-@onready var button_load: Button = $VBoxContainer/Button_Load
+@onready var button_save: Button = $Control/HBoxContainer/Button_Save
+@onready var button_load: Button = $Control/HBoxContainer/Button_Load
 
 var is_paused : bool = false
 
